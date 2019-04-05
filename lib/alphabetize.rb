79702a1b25c133f16857105ad5088c65e0c14767
Matt Williams <matt.seongjun@gmail.com>
2019-04-04 23:28:51 -0400

    Sort and sort_by need better documentation

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+    guide = ESPERANTO_ALPHABET.chars
+    arr.sort do |string_a, string_b|
+        string_a.split("").map{|c| guide.index(c)} <=> string_b.split("").map{|c| guide.index(c)}
+    end
 end
