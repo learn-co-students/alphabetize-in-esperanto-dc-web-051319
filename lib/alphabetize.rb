ac24f095b83f1ecd128c79357173ad5aaac18d1b
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-10 23:29:16 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,37 +1,36 @@
 require 'pry'
 
-
-
-
 def alphabetize(arr)
-  e_alphabet = ""
-  e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  hash_alphabet = {}
-  array_final = [] 
+e_alphabet = ""
+e_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+hash_alphabet = {}
+array_final = []
 
-  e_alphabet.split("").each_with_index do |letter, idx|
-    hash_alphabet[letter] = {:idx => []}
-  end 
- 
-  arr.each do |word|
+e_alphabet.split("").each_with_index do |letter, idx |
+    hash_alphabet[letter] = {
+      :idx => []
+    }
+  end
+
+arr.each do |word |
     first_letter = ""
-    first_letter = word[0]
-    hash_alphabet[first_letter][:idx] = hash_alphabet[first_letter][:idx].push(word) 
-    hash_alphabet[first_letter][:idx] = hash_alphabet[first_letter][:idx].sort
-  end 
-
-  hash_alphabet.each do |k, data|
-    data.each do |idx, str|
-     array_final = array_final.push(str)
-     
+  first_letter = word[0]
+hash_alphabet[first_letter][:idx] = hash_alphabet[first_letter][:idx].push(word)
+hash_alphabet[first_letter][:idx] = hash_alphabet[first_letter][:idx].sort
 end
-end 
-array_final = array_final.delete_if {|x| x==[]}
+
+hash_alphabet.each do |k, data |
+    data.each do |idx, str |
+      array_final = array_final.push(str)
+
+    end
+  end
+array_final = array_final.delete_if {
+  | x | x == []
+}
 
 array_final = array_final.flatten
 binding.pry
 return array_final
-end 
-
-  
+end
   
